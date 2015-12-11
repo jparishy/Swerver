bab076538dc1d57e4e3f2cbbb8c1d6dda54095e7
Julius Parishy <hello@juliusparishy.com>
2015-12-11 16:20:06 -0500

    update build script for linux

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
+
+MODULE_NAME=Swerver
+
 clang -c ./Sources/Bridge.c
-swiftc ./Sources/*.swift -Xlinker Bridge.o -import-objc-header ./Sources/BridgingHeader.h -Xcc -I/usr/include -Xcc -I/usr/include/x86_64-linux-gnu -Xcc -iquote -module-name Swerver -Xcc -working-directory/home/jp/code/swerver -Xcc -I/home/jp/code/swerver/Sources -Xcc -Werror -luv
+swiftc ./Sources/*.swift -Xlinker Bridge.o -import-objc-header ./Sources/BridgingHeader.h -Xcc -I/usr/include -Xcc -I/usr/include/x86_64-linux-gnu -Xcc -iquote -module-name $MODULE_NAME -Xcc -working-directory`pwd` -Xcc -I`pwd`/Swerver -Xcc -Werror -luv
