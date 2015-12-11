d2678d44a16d5e1a2f40f3b02fd2c0968b93ba06
Julius Parishy <hello@juliusparishy.com>
2015-12-11 16:23:27 -0500

    updates to the build sript

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 MODULE_NAME=Swerver
+SOURCES_DIR=./Swerver
 
-clang -c ./Sources/Bridge.c
-swiftc ./Sources/*.swift -Xlinker Bridge.o -import-objc-header ./Sources/BridgingHeader.h -Xcc -I/usr/include -Xcc -I/usr/include/x86_64-linux-gnu -Xcc -iquote -module-name $MODULE_NAME -Xcc -working-directory`pwd` -Xcc -I`pwd`/Swerver -Xcc -Werror -luv
+clang -c $SOURCES_DIR/Bridge.c -o .build/Bridge.o
+swiftc $SOURCES_DIR/*.swift -Xlinker .build/Bridge.o -import-objc-header $SOURCES_DIR/BridgingHeader.h -Xcc -I/usr/include -Xcc -I/usr/include/x87_64-linux-gnu -Xcc -iquote -module-name $MODULE_NAME -Xcc -working-directory"`pwd`" -Xcc -I"`pwd`/$SOURCES_DIR" -Xcc -Werror -luv
