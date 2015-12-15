511291992e326ce600c6550db451aad490aefbb5
Julius Parishy <hello@juliusparishy.com>
2015-12-15 01:09:11 -0500

    fix issues with querying on linux

--- Examples/SwerverTodoMVC/Controllers/TodosController.swift
+++ Examples/SwerverTodoMVC/Controllers/TodosController.swift
@@ -61,7 +61,7 @@ class TodosController : Controller {
             return (.Ok, [:], nil)
         } else {
             let query = ModelQuery<Todo>(transaction: t)
-            let toDelete = try query.findWhere(["completed":true])
+            let toDelete = try query.findWhere(["completed": JSONBool(bool: true)])
             
             for m in toDelete {
                 try query.delete(NSNumber(integer: Int(m.id)))
