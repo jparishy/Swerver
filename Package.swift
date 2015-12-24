3d536d23a757c3e3054f2fbab0def71de7024c0a
Julius Parishy <hello@juliusparishy.com>
2015-12-23 20:33:54 -0500

    fixed package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-	name: "Swerver"
+	name: "Swerver",
+	dependencies: [
+		.Package(url: "../Dependencies/libpq", majorVersion: 1),
+		.Package(url: "../Dependencies/libuv", majorVersion: 1),
+		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
+	]
 )
