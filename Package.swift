c41a6f0b538081b8bd5ba27126f2922b490fd6d4
Julius Parishy <hello@juliusparishy.com>
2015-12-23 22:44:59 -0500

    update package to use the remote libuv and libpq modules

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
 	name: "Swerver",
 	dependencies: [
-		.Package(url: "../Dependencies/libpq", majorVersion: 1),
-		.Package(url: "../Dependencies/libuv", majorVersion: 1),
+		.Package(url: "https://github.com/Swerver/libuv-swift.git", majorVersion: 1),
+		.Package(url: "https://github.com/Swerver/libpq-swift.git", majorVersion: 1),
 		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0)
 	]
 )
