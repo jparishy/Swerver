fe58d289899bc7c1f2392c45bb4fcaabf6a583c1
Julius Parishy <hello@juliusparishy.com>
2015-12-27 22:58:01 -0500

    add unit test for the controller crud action routing

--- Tests/Controllers/ControllerTests.swift
+++ Tests/Controllers/ControllerTests.swift
@@ -7,9 +7,56 @@ import CryptoSwift
 
 class TestController : Controller {
 	var handleIndex: ControllerRequestHandler? = nil
+	var handleShow: ControllerRequestHandler? = nil
+	var handleNew: ControllerRequestHandler? = nil
+	var handleCreate: ControllerRequestHandler? = nil
+	var handleUpdate: ControllerRequestHandler? = nil
+	var handleDelete: ControllerRequestHandler? = nil
+	var handleNamespaceIdentity: ControllerRequestHandler? = nil
+
 	override func index(request: Request, parameters: Parameters, session inSession: Session, transaction t: Transaction?) throws -> ControllerResponse {
 	    if let index = handleIndex {
 	    	return try index(request: request, parameters: parameters, session: inSession, transaction: t)
+	    } else {
+	    	throw UserError.Unimplemented
+	    }
+	}
+
+	override func show(request: Request, parameters: Parameters, session inSession: Session, transaction t: Transaction?) throws -> ControllerResponse {
+	    if let show = handleShow {
+	    	return try show(request: request, parameters: parameters, session: inSession, transaction: t)
+	    } else {
+	    	throw UserError.Unimplemented
+	    }
+	}
+
+	override func new(request: Request, parameters: Parameters, session inSession: Session, transaction t: Transaction?) throws -> ControllerResponse {
+	    if let new = handleNew {
+	    	return try new(request: request, parameters: parameters, session: inSession, transaction: t)
+	    } else {
+	    	throw UserError.Unimplemented
+	    }
+	}
+
+	override func create(request: Request, parameters: Parameters, session inSession: Session, transaction t: Transaction?) throws -> ControllerResponse {
+	    if let create = handleCreate {
+	    	return try create(request: request, parameters: parameters, session: inSession, transaction: t)
+	    } else {
+	    	throw UserError.Unimplemented
+	    }
+	}
+
+	override func update(request: Request, parameters: Parameters, session inSession: Session, transaction t: Transaction?) throws -> ControllerResponse {
+	    if let update = handleUpdate {
+	    	return try update(request: request, parameters: parameters, session: inSession, transaction: t)
+	    } else {
+	    	throw UserError.Unimplemented
+	    }
+	}
+
+	override func delete(request: Request, parameters: Parameters, session inSession: Session, transaction t: Transaction?) throws -> ControllerResponse {
+	    if let delete = handleDelete {
+	    	return try delete(request: request, parameters: parameters, session: inSession, transaction: t)
 	    } else {
 	    	throw UserError.Unimplemented
 	    }
@@ -145,13 +192,73 @@ class ControllerTests : XCTestCase, XCTestCaseProvider {
 			XCTFail("Should not throw")
 		}
 	}
+
+	func testAllCRUDActions() {
+		let app = TestApplication()
+
+		let c = TestController(application: app)
+		c.resource = Resource(name: "test", controller: c)
+
+		c.handleIndex = {
+			(r: Request, p: Parameters, s: Session, t: Transaction?) throws -> ControllerResponse in
+			return ControllerResponse(.Ok)
+		}
+
+		c.handleShow = {
+			(r: Request, p: Parameters, s: Session, t: Transaction?) throws -> ControllerResponse in
+			return ControllerResponse(.Ok)
+		}
+
+		c.handleNew = {
+			(r: Request, p: Parameters, s: Session, t: Transaction?) throws -> ControllerResponse in
+			return ControllerResponse(.Ok)
+		}
+
+		c.handleCreate = {
+			(r: Request, p: Parameters, s: Session, t: Transaction?) throws -> ControllerResponse in
+			return ControllerResponse(.Ok)
+		}
+
+		c.handleUpdate = {
+			(r: Request, p: Parameters, s: Session, t: Transaction?) throws -> ControllerResponse in
+			return ControllerResponse(.Ok)
+		}
+
+		c.handleDelete = {
+			(r: Request, p: Parameters, s: Session, t: Transaction?) throws -> ControllerResponse in
+			return ControllerResponse(.Ok)
+		}
+
+		let datas = [
+			MakeHTTPRequest("GET", path: "/test", headers: [:], body: ""),
+			MakeHTTPRequest("GET", path: "/test/42", headers: [:], body: ""),
+			MakeHTTPRequest("GET", path: "/test/new", headers: [:], body: ""),
+			MakeHTTPRequest("POST", path: "/test", headers: [:], body: ""),
+			MakeHTTPRequest("PUT", path: "/test/42", headers: [:], body: ""),
+			MakeHTTPRequest("DELETE", path: "/test/42", headers: [:], body: ""),
+		]
+
+		for data in datas {
+			do {
+				if let request = HTTP11(rawRequest: data).request() {
+					let response = try c.apply(request)
+					XCTAssertEqual(response.statusCode.integerCode, StatusCode.Ok.integerCode, "Should have gotten a 200 response code")
+				} else {
+					XCTFail("Could not make request")
+				}
+			} catch {
+				XCTFail("Should have been able to route the request")
+			}
+		}
+	}
 }
 
 extension ControllerTests {
 	var allTests : [(String, () -> Void)] {
 		return [
 			("testParameters", testParameters),
-			("testCookies", testCookies)
+			("testCookies", testCookies),
+			("testAllCRUDActions", testAllCRUDActions)
 		]
 	}
 }
